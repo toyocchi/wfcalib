f4154040b7449bea2a9e55dcf1c047a4cb8eecc8
satoruk-icepp <satoruk@icepp.s.u-tokyo.ac.jp>
2018-08-04 01:13:21 +0200

    dark noise implementation and code clean up

--- StatGainCalib.cpp
+++ StatGainCalib.cpp
@@ -12,17 +12,227 @@ Double_t MultipleError(Double_t &value1, Double_t &value1err, Double_t &value2, 
 // void Differentiate(Double_t *wf,Int_t ndiff);
 Double_t fitfunc(Double_t *x, Double_t *par);
 
-
-TGraph* grQNQWF=new TGraph();
-Double_t grQNQange[2] = {-100, 1000};
-
-Int_t Nrep=5;
-
-Double_t WFT[gNbin]    = {};
 void DisplayNode(TXMLEngine* xml, XMLNodePointer_t node, Int_t level);
 void InitConfig(std::string &linestr,std::string &strvalue);
 void ParseConfig(TXMLEngine* xml, XMLNodePointer_t node);
 // TFile* fResult=new TFile("Result.root","recreate");
+
+void Init(void) {
+   TXMLEngine* xml = new TXMLEngine;
+
+   XMLDocPointer_t xmldoc = xml->ParseFile("./SimConfig.xml");
+   if (xmldoc==0) {
+      delete xml;
+      return;
+   }
+   XMLNodePointer_t mainnode = xml->DocGetRootElement(xmldoc);
+   // display recursively all nodes and subnodes
+   // DisplayNode(xml, mainnode, 1);
+   ParseConfig(xml, mainnode);
+   // Release memory before exit
+   xml->FreeDoc(xmldoc);
+   delete xml;
+
+   // LoadSimConfig();
+   // std::cout<<"ScintDecay: "<<ScintDecay<<std::endl;
+   gFSctime->SetParameter(0,ScintDecay);
+   gFSingle->SetParameter(0,SPwidth);
+   gFSingle->SetParameter(1,0);
+   gFSingle->FixParameter(2,Gain);
+   gAPtime ->FixParameter(0,APtimeconstant);
+}
+
+void StatGainCalib(void) {
+   TCanvas *cgr0 =new TCanvas("cgr0", "cgr0",600,600);
+   TCanvas *cgr1 =new TCanvas("cgr1", "cgr1",600,600);
+   // TCanvas *cgrQNvar =new TCanvas("cgrQNvar", "cgrQNvar",600,600);
+   Init();
+   std::vector<Double_t> vecSum;
+   std::vector<std::vector<Double_t>> vecNoise;
+   std::vector<std::vector<Double_t>> vecInterference;
+
+
+   TFile* fout = new TFile("fout.root","recreate");
+   TTree* tout = new TTree("tout","tout");
+   Double_t charge;
+   Double_t noisevar;
+   Double_t noiselevel;
+   tout->Branch("charge"    ,&charge    ,"charge/D");
+   tout->Branch("noisevar"  ,&noisevar  ,"noisevar/D");
+   tout->Branch("noiselevel",&noiselevel,"noiselevel/D");
+
+   Double_t sum;
+
+   Int_t npheRange[2] = {RangeMin,RangeMax};
+
+   Double_t logstep = (TMath::Log(npheRange[1]) - TMath::Log(npheRange[0]))/ (Nstep -1);
+
+
+   TClonesArray* cagrQNvar = new TClonesArray("TGraphErrors",noiselist.size());
+   // TClonesArray* cagrQNQ   = new TClonesArray("TGraph",noiselist.size());
+   // TClonesArray* cagrInt   = new TClonesArray("TGraph",noiselist.size());
+   // TClonesArray* cahQNQ    = new TClonesArray("TH2D",noiselist.size());
+   TClonesArray* cafpol1    = new TClonesArray("TF1",noiselist.size());
+   // TF1* fpol1= new TF1("fpol1","[0]*x+[1]");
+   for (int i = 0; i < noiselist.size(); i++) {
+      new ((TGraphErrors*)(*cagrQNvar)[i]) TGraphErrors();
+      // new ((TGraph*)(*cagrQNQ)[i])   TGraph();
+      // new ((TGraph*)(*cagrInt)[i])   TGraph();
+      // new ((TH2D*)(*cahQNQ)[i])   TH2D(Form("hQNQ%d",i),Form("hQNQ%d",i),100,0,5000,100,0,5);
+      new ((TF1*)(*cafpol1)[i])   TF1(Form("fpol1%d",i),"[0]*x+[1]");
+   }
+
+
+   for (int istep = 0; istep < Nstep; istep++) {
+      // int iphe = (int)TMath::Exp(TMath::Log(npheRange[0]) + logstep * istep);
+      int iphe = istep*(npheRange[1]-npheRange[0])/Nstep;
+      for(int irep=0;irep<Nevent;irep++){
+         // std::vector<Double_t> noisevar;
+         // std::vector<Double_t> inter;
+         cout<<istep<<" "<<iphe<<endl;
+
+         for (int i = 0; i < noiselist.size(); i++) {
+            noiselevel=noiselist[i];
+            Waveform* wf= new Waveform(Nbins,-100,1000);
+            wf->SetDarkNoiseFrequency(DNFreq);
+            wf->MakeEvent(iphe);
+            wf->MakeDarkNoise();
+            wf->SetNoiseLevel(noiselevel);
+            wf->MakeElectricNoise();
+            charge  = wf->GetCharge();
+            wf->Differentiate(Ndiff);
+            noisevar = wf->GetTotalVariance();
+
+            Int_t index=istep*Nevent+irep;
+            ((TGraphErrors*)(*cagrQNvar)[i])->SetPoint(     index,charge,noisevar);
+            ((TGraphErrors*)(*cagrQNvar)[i])->SetPointError(index,0     ,noisevar*0.2);
+            // if (wf->GetNDN()>0) {
+            //    // std::cout<<"NDN: "<<wf->GetNDN()<<std::endl;
+            //    wf->Draw();
+            // }
+
+            delete wf;
+            tout->Fill();
+         }
+      }
+   }
+   fout->cd();
+   tout->Write();
+   fout->Close();
+   TString fname="gain1_noise5e-2.pdf";
+
+   TF1* func= new TF1("fitfunc",fitfunc,-10,5000,3);
+
+   cgr0->cd();
+   for (int i = 0; i < noiselist.size(); i++) {
+
+      Double_t par[3];
+      Double_t err[3];
+      // Double_t YACut=YCut(((TGraph*)(*cagrQNvar)[i]));
+      TF1* pol1= new TF1("fitfunc",fitfunc,-100,5000,3);
+      // pol1->SetParLimits(2,0,1);
+      // pol1->SetParLimits(0,-1,20);
+      ((TGraphErrors*)(*cagrQNvar)[i])->Fit("fitfunc","","");
+      // Double_t par[3];
+      for (int i = 0; i < 3; i++) {
+         par[i]=pol1->GetParameter(i);
+         err[i]=pol1->GetParError(i);
+      }
+
+      Double_t MesGain=par[1]/(1-par[1]*par[2]);
+      Double_t pmul=par[1]*par[2];
+      Double_t pmulerr=MultipleError(par[1],err[1],par[2],err[2]);
+      Double_t MesGainerr=DivisionError(par[1],err[1],(double)1-pmul,pmulerr);
+      std::cout<<"Gain: "<<MesGain<<"+-"<<MesGainerr<<std::endl;
+
+      std::cout<<"Pmul: "<<pmul<<"+-"<<pmulerr<<std::endl;
+      // ((TGraph*)(*cagrQNvar)[i])->Fit("fitfunc","","");
+      ((TGraphErrors*)(*cagrQNvar)[i])->SetTitle("Q_{dint}vs Q^{2}_{drms};Q_{dint};Q^{2}_{drms}");
+      ((TGraph*)(*cagrQNvar)[i])->SetMaximum(500);
+      ((TGraphErrors*)(*cagrQNvar)[i])->SetMinimum(0);
+      ((TGraphErrors*)(*cagrQNvar)[i])->SetMarkerStyle(20);
+      ((TGraphErrors*)(*cagrQNvar)[i])->SetMarkerColor(2+i);
+      if (i==0) {
+         ((TGraphErrors*)(*cagrQNvar)[i])->Draw("ap");
+      }else{
+         ((TGraphErrors*)(*cagrQNvar)[i])->Draw("p same");
+      }
+   }
+}
+
+Double_t fitfunc(Double_t *x, Double_t *par){
+   //par[0]: offset;
+   //par[1]:
+   Double_t  xx        = x[0];
+   // Double_t PMultiple = TMath::Sqrt(TMath::Power(par[2]*xx,2)/1+TMath::Power(par[2]*xx,2));
+   // Double_t PMultiple = par[2];
+   // Double_t Gain = 1;
+   return par[0]+par[1]*(xx+par[2]*xx*xx);
+}
+
+Double_t DivisionError(Double_t &value1, Double_t &value1err,const Double_t &value2, Double_t &value2err){
+   /*calculate error of value 1/ value 2*/
+   return TMath::Sqrt(TMath::Power(value1err/value2,2)+TMath::Power(value1*value2err/(value2*value2),2));
+}
+
+Double_t MultipleError(Double_t &value1, Double_t &value1err, Double_t &value2, Double_t &value2err){
+   /*calculate error of value 1* value 2*/
+   return TMath::Sqrt(TMath::Power(value1err* value2,2)+TMath::Power(value1*value2err,2));
+}
+
+// void LoadSimConfig(){
+//    ifstream fsimconf;
+//    TString simconfname = "./SimConfig.dat";
+//    std::cout<<"Loading Configuration..."<<std::endl;
+//    fsimconf.open(simconfname.Data(),std::ios::in);
+//    string linestr;
+//    while (!fsimconf.eof()) {
+//       fsimconf>>linestr;
+//       std::cout<<linestr<<std::endl;
+//       // string str( "abcdefghijk" );
+//       char key = ':';
+//       Int_t colonpos=linestr.find(key);
+//       string strvalue = linestr.substr(colonpos+1);
+//       // std::cout << colonpos << std::endl;
+//       // std::cout <<  << std::endl;
+//       if (linestr.find(strLambda)!=string::npos) {
+//          lambda = std::stod(strvalue);
+//       }
+//       if (linestr.find(strAlpha)!=string::npos) {
+//          alpha = std::stod(strvalue);
+//       }
+//       if (linestr.find(strScintDecay)!=string::npos) {
+//          ScintDecay = std::stod(strvalue);
+//       }
+//       if (linestr.find(strSPwidth)!=string::npos) {
+//          SPwidth = std::stod(strvalue);
+//       }
+//       if (linestr.find(strAPtimeconstant)!=string::npos) {
+//          APtimeconstant = std::stod(strvalue);
+//       }
+//       if (linestr.find(strGain)!=string::npos) {
+//          Gain = std::stod(strvalue);
+//       }
+//       if (linestr.find(strRangeMin)!=string::npos) {
+//          RangeMin = std::stoi(strvalue);
+//       }
+//       if (linestr.find(strRangeMax)!=string::npos) {
+//          RangeMax = std::stoi(strvalue);
+//       }
+//       if (linestr.find(strNstep)!=string::npos) {
+//          Nstep = std::stoi(strvalue);
+//       }
+//       if (linestr.find(strNdiff)!=string::npos) {
+//          Ndiff = std::stoi(strvalue);
+//       }
+//       if (linestr.find(strNevent)!=string::npos) {
+//          Nevent = std::stoi(strvalue);
+//       }
+//       if (linestr.find(strNoiseLevel)!=string::npos) {
+//          noiselist.push_back(std::stod(strvalue));
+//       }
+//    }
+// }
 
 void DisplayNode(TXMLEngine* xml, XMLNodePointer_t node, Int_t level)
 {
@@ -101,207 +311,7 @@ void InitConfig(std::string &linestr,std::string &strvalue){
    if (linestr.find(strNstep)         !=string::npos)          Nstep = std::stoi(strvalue);
    if (linestr.find(strNdiff)         !=string::npos)          Ndiff = std::stoi(strvalue);
    if (linestr.find(strNevent)        !=string::npos)         Nevent = std::stoi(strvalue);
+   if (linestr.find(strDNFreq)        !=string::npos)         DNFreq = std::stod(strvalue);
+   if (linestr.find(strNbins)         !=string::npos)         Nbins  = std::stoi(strvalue);
    if (linestr.find(strNoiseLevel)    !=string::npos)     noiselist.push_back(std::stod(strvalue));
 }
-
-void Init(void) {
-   TXMLEngine* xml = new TXMLEngine;
-
-   XMLDocPointer_t xmldoc = xml->ParseFile("./data.xml");
-   if (xmldoc==0) {
-      delete xml;
-      return;
-   }
-   XMLNodePointer_t mainnode = xml->DocGetRootElement(xmldoc);
-   // display recursively all nodes and subnodes
-   // DisplayNode(xml, mainnode, 1);
-   ParseConfig(xml, mainnode);
-   // Release memory before exit
-   xml->FreeDoc(xmldoc);
-   delete xml;
-
-   // LoadSimConfig();
-   std::cout<<"ScintDecay: "<<ScintDecay<<std::endl;
-   gFSctime->SetParameter(0,ScintDecay);
-   gFSingle->SetParameter(0,SPwidth);
-   gFSingle->SetParameter(1,0);
-   gFSingle->FixParameter(2,Gain);
-   gAPtime ->FixParameter(0,APtimeconstant);
-   // for (int iBin = 0; iBin < gNbin; iBin++) WFT[iBin] = (grQNQange[1] - grQNQange[0]) /gNbin * iBin + grQNQange[0];
-}
-
-void StatGainCalib(void) {
-   TCanvas *cgr0 =new TCanvas("cgr0", "cgr0",600,600);
-   TCanvas *cgr1 =new TCanvas("cgr1", "cgr1",600,600);
-   // TCanvas *cgrQNvar =new TCanvas("cgrQNvar", "cgrQNvar",600,600);
-   Init();
-   std::vector<Double_t> vecSum;
-   std::vector<std::vector<Double_t>> vecNoise;
-   std::vector<std::vector<Double_t>> vecInterference;
-
-   Double_t sum;
-
-   Int_t npheRange[2] = {RangeMin,RangeMax};
-
-   Double_t logstep = (TMath::Log(npheRange[1]) - TMath::Log(npheRange[0]))/ (Nstep -1);
-
-
-   TClonesArray* cagrQNvar = new TClonesArray("TGraphErrors",noiselist.size());
-   TClonesArray* cagrQNQ   = new TClonesArray("TGraph",noiselist.size());
-   // TClonesArray* cagrInt   = new TClonesArray("TGraph",noiselist.size());
-   TClonesArray* cahQNQ    = new TClonesArray("TH2D",noiselist.size());
-   TClonesArray* cafpol1    = new TClonesArray("TF1",noiselist.size());
-   // TF1* fpol1= new TF1("fpol1","[0]*x+[1]");
-   for (int i = 0; i < noiselist.size(); i++) {
-      new ((TGraphErrors*)(*cagrQNvar)[i]) TGraphErrors();
-      new ((TGraph*)(*cagrQNQ)[i])   TGraph();
-      // new ((TGraph*)(*cagrInt)[i])   TGraph();
-      // new ((TH2D*)(*cahQNQ)[i])   TH2D(Form("hQNQ%d",i),Form("hQNQ%d",i),100,0,5000,100,0,5);
-      new ((TF1*)(*cafpol1)[i])   TF1(Form("fpol1%d",i),"[0]*x+[1]");
-   }
-
-
-   for (int istep = 0; istep < Nstep; istep++) {
-      // int iphe = (int)TMath::Exp(TMath::Log(npheRange[0]) + logstep * istep);
-      int iphe = istep*(npheRange[1]-npheRange[0])/Nstep;
-      for(int irep=0;irep<Nrep;irep++){
-         std::vector<Double_t> noisevar;
-         std::vector<Double_t> inter;
-         // cout<<istep<<" "<<iphe<<" sum : "<<sum<<endl;
-
-         for (int i = 0; i < noiselist.size(); i++) {
-            Waveform* wf= new Waveform(gNbin,-100,1000);
-            wf->MakeEvent(iphe);
-
-            wf->SetNoiseLevel(noiselist[i]);
-            Double_t charge=wf->GetCharge();
-            wf->Differentiate(Ndiff);
-            Double_t noisevar=wf->GetTotalVariance();
-            // std::cout<<"variance: "<<noisevar<<std::endl;
-            // noisevar.push_back(wf->GetTotalVariance());
-            // inter.push_back(wf->GetInterference());
-            Int_t index=istep*Nrep+irep;
-            ((TGraphErrors*)(*cagrQNvar)[i])->SetPoint(     index,charge,noisevar);
-            ((TGraphErrors*)(*cagrQNvar)[i])->SetPointError(index,0     ,noisevar*0.2);
-            delete wf;
-         }
-         vecNoise.push_back(noisevar);
-         vecInterference.push_back(inter);
-      }
-   }
-
-   TString fname="gain1_noise5e-2.pdf";
-
-   TF1* func= new TF1("fitfunc",fitfunc,-10,5000,3);
-
-   cgr0->cd();
-   for (int i = 0; i < noiselist.size(); i++) {
-
-      Double_t par[3];
-      Double_t err[3];
-      // Double_t YACut=YCut(((TGraph*)(*cagrQNvar)[i]));
-      TF1* pol1= new TF1("fitfunc",fitfunc,-100,5000,3);
-      // pol1->SetParLimits(2,0,1);
-      // pol1->SetParLimits(0,-1,20);
-      ((TGraphErrors*)(*cagrQNvar)[i])->Fit("fitfunc","","");
-      // Double_t par[3];
-      for (int i = 0; i < 3; i++) {
-         par[i]=pol1->GetParameter(i);
-         err[i]=pol1->GetParError(i);
-      }
-
-      Double_t MesGain=par[1]/(1-par[1]*par[2]);
-      Double_t pmul=par[1]*par[2];
-      Double_t pmulerr=MultipleError(par[1],err[1],par[2],err[2]);
-      Double_t MesGainerr=DivisionError(par[1],err[1],(double)1-pmul,pmulerr);
-      std::cout<<"Gain: "<<MesGain<<"+-"<<MesGainerr<<std::endl;
-
-      std::cout<<"Pmul: "<<pmul<<"+-"<<pmulerr<<std::endl;
-      // ((TGraph*)(*cagrQNvar)[i])->Fit("fitfunc","","");
-      ((TGraphErrors*)(*cagrQNvar)[i])->SetTitle("Q_{dint}vs Q^{2}_{drms};Q_{dint};Q^{2}_{drms}");
-      ((TGraph*)(*cagrQNvar)[i])->SetMaximum(500);
-      ((TGraphErrors*)(*cagrQNvar)[i])->SetMinimum(0);
-      ((TGraphErrors*)(*cagrQNvar)[i])->SetMarkerStyle(20);
-      ((TGraphErrors*)(*cagrQNvar)[i])->SetMarkerColor(2+i);
-      if (i==0) {
-         ((TGraphErrors*)(*cagrQNvar)[i])->Draw("ap");
-      }else{
-         ((TGraphErrors*)(*cagrQNvar)[i])->Draw("p same");
-      }
-   }
-}
-
-Double_t fitfunc(Double_t *x, Double_t *par){
-   //par[0]: offset;
-   //par[1]:
-   Double_t  xx        = x[0];
-   // Double_t PMultiple = TMath::Sqrt(TMath::Power(par[2]*xx,2)/1+TMath::Power(par[2]*xx,2));
-   // Double_t PMultiple = par[2];
-   // Double_t Gain = 1;
-   return par[0]+par[1]*(xx+par[2]*xx*xx);
-}
-
-Double_t DivisionError(Double_t &value1, Double_t &value1err,const Double_t &value2, Double_t &value2err){
-   /*calculate error of value 1/ value 2*/
-   return TMath::Sqrt(TMath::Power(value1err/value2,2)+TMath::Power(value1*value2err/(value2*value2),2));
-}
-
-Double_t MultipleError(Double_t &value1, Double_t &value1err, Double_t &value2, Double_t &value2err){
-   /*calculate error of value 1* value 2*/
-   return TMath::Sqrt(TMath::Power(value1err* value2,2)+TMath::Power(value1*value2err,2));
-}
-
-void LoadSimConfig(){
-   ifstream fsimconf;
-   TString simconfname = "./SimConfig.dat";
-   std::cout<<"Loading Configuration..."<<std::endl;
-   fsimconf.open(simconfname.Data(),std::ios::in);
-   string linestr;
-   while (!fsimconf.eof()) {
-      fsimconf>>linestr;
-      std::cout<<linestr<<std::endl;
-      // string str( "abcdefghijk" );
-      char key = ':';
-      Int_t colonpos=linestr.find(key);
-      string strvalue = linestr.substr(colonpos+1);
-      // std::cout << colonpos << std::endl;
-      // std::cout <<  << std::endl;
-      if (linestr.find(strLambda)!=string::npos) {
-         lambda = std::stod(strvalue);
-      }
-      if (linestr.find(strAlpha)!=string::npos) {
-         alpha = std::stod(strvalue);
-      }
-      if (linestr.find(strScintDecay)!=string::npos) {
-         ScintDecay = std::stod(strvalue);
-      }
-      if (linestr.find(strSPwidth)!=string::npos) {
-         SPwidth = std::stod(strvalue);
-      }
-      if (linestr.find(strAPtimeconstant)!=string::npos) {
-         APtimeconstant = std::stod(strvalue);
-      }
-      if (linestr.find(strGain)!=string::npos) {
-         Gain = std::stod(strvalue);
-      }
-      if (linestr.find(strRangeMin)!=string::npos) {
-         RangeMin = std::stoi(strvalue);
-      }
-      if (linestr.find(strRangeMax)!=string::npos) {
-         RangeMax = std::stoi(strvalue);
-      }
-      if (linestr.find(strNstep)!=string::npos) {
-         Nstep = std::stoi(strvalue);
-      }
-      if (linestr.find(strNdiff)!=string::npos) {
-         Ndiff = std::stoi(strvalue);
-      }
-      if (linestr.find(strNevent)!=string::npos) {
-         Nevent = std::stoi(strvalue);
-      }
-      if (linestr.find(strNoiseLevel)!=string::npos) {
-         noiselist.push_back(std::stod(strvalue));
-      }
-   }
-}
-
--- Waveform.h
+++ Waveform.h
@@ -22,9 +22,14 @@ Int_t    Ndiff;
 string   strNdiff="Ndiff";
 Int_t    Nevent;
 string   strNevent  ="Nevent";
+Double_t    DNFreq;
+string   strDNFreq  ="DarkNoiseFrequency";
+Int_t    Nbins;
+string   strNbins  ="Nbins";
 // Int_t    Nevent;
 std::vector<Double_t> noiselist;
 string   strNoiseLevel  ="NoiseLevel";
+TString safeName(TString name);
 Double_t funcsctime(Double_t *x, Double_t *par);
 Double_t funcsingle(Double_t *x, Double_t *par);
 Double_t APtiming(Double_t *x, Double_t *par);
@@ -33,7 +38,13 @@ Int_t Borel_gen(Double_t); // generate the random variable obeying Borel
 TF1 *gFSingle = new TF1("fsingle", funcsingle, timemin, timemax, 3);
 TF1 *gFSctime = new TF1("fsctime", funcsctime, timemin, timemax, 1);
 TF1 *gAPtime = new TF1("APtime", APtiming, 0, timemax, 1);
-const Int_t gNbin = 1100;
+
+TString safeName(TString name){
+	TObject* old = gROOT->FindObject(name);
+	if(old) delete old;
+	return name;
+}
+
 Double_t funcsctime(Double_t *x, Double_t *par) {
    //par 0: decay time const
    if (x[0] < 0) return 0;
@@ -60,20 +71,20 @@ Double_t Borel(Double_t lambda, Int_t n){
    Double_t exp = TMath::Exp(-lambda*n);
    Double_t power = 1.;
    for (Int_t i=1;i<n;++i){
-     power *= lambda*n;
+      power *= lambda*n;
    }
    Double_t numerator = TMath::Factorial(n);
    return exp*power/numerator;
 }
 
 Int_t Borel_gen(Double_t lambda){
-  Int_t return_rn = 0;
-  Double_t rand_uni = gRandom->Uniform();
-  while(rand_uni > 0){
-    return_rn++;
-    rand_uni -= Borel(lambda,return_rn);
-  }
-  return return_rn;
+   Int_t return_rn = 0;
+   Double_t rand_uni = gRandom->Uniform();
+   while(rand_uni > 0){
+      return_rn++;
+      rand_uni -= Borel(lambda,return_rn);
+   }
+   return return_rn;
 }
 
 
@@ -89,6 +100,9 @@ protected:
    Double_t        *fTime;
    Double_t        fTimeMin;      //Time of the first (0) point used for fix bin
    Double_t        fTimeMax;
+   Double_t        fDNFreq=0;
+   Double_t        fNoiseLevel=0;
+   Int_t           fNDN;
 public:
    // Double_t variance;
    Waveform(Int_t npoints,Double_t timemin,Double_t timemax);
@@ -97,7 +111,11 @@ public:
    void SetAmplitudeAt(Int_t ibin,Double_t amp);
    void Differentiate(Int_t ndiff);
    void MakeEvent(Int_t npe);
-   void SetNoiseLevel(Double_t noiselevel);
+   void MakeDarkNoise();
+   void MakeElectricNoise();
+   void SetNoiseLevel(Double_t noiselevel){fNoiseLevel=noiselevel;};
+   void SetDarkNoiseFrequency(Double_t DNfreq){fDNFreq=DNfreq;};
+   void Draw();
    Double_t GetAmplitude(Int_t ibin);
    Double_t GetTimeAt(Int_t ibin);
    Double_t GetTotalVariance();
@@ -105,6 +123,7 @@ public:
    Double_t GetCharge();
    Double_t GetNoiseConst();
    Double_t GetInterference();
+   Int_t    GetNDN(){return fNDN;};
 
 };
 
@@ -129,15 +148,6 @@ Waveform::~Waveform(){
    delete[] fTime;
    delete[] fAmplitude;
    delete[] fNoiseAmp;
-   // fTimeMin   = timemin;
-   // fTimeMax   = timemax;
-   // memset(fAmplitude, 0, sizeof(Double_t)*npoints);
-   // memset(fNoiseAmp,  0, sizeof(Double_t)*npoints);
-   // for (int ipnt = 0; ipnt < npoints; ipnt++) {
-   //    fTime[ipnt]=timemin+(timemax-timemin)*ipnt/fNPoints;
-   // }
-   // fBinsize= (fTimeMax-fTimeMin)/(Double_t)fNPoints;
-   // std::cout<<"fbinsize : "<<fBinsize<<" fNPoints: "<<fNPoints<<std::endl;
 }
 
 void Waveform::SetAmplitudeAt(Int_t ibin,Double_t amp){
@@ -162,6 +172,15 @@ Double_t Waveform::GetTimeAt(Int_t ibin){
 // Waveform* diffwf= new Waveform();
 
 // }
+void Waveform::Draw(){
+   TCanvas* canvastemp= new TCanvas(safeName("canvastemp"),safeName("canvastemp"),600,600);
+   TGraph* grwf= new TGraph();
+   for (Int_t ipoint = 0; ipoint < fNPoints; ipoint++) {
+      grwf->SetPoint(ipoint,fTime[ipoint],fAmplitude[ipoint]);
+   }
+   grwf->Draw("ap");
+
+}
 
 void Waveform::Differentiate(Int_t ndiff){
 
@@ -173,21 +192,21 @@ void Waveform::Differentiate(Int_t ndiff){
       memcpy(tempWF,fAmplitude,fNPoints*sizeof(Double_t) );
       memcpy(tempNWF,fNoiseAmp,fNPoints*sizeof(Double_t) );
 
-   for(int idiff=0;idiff<ndiff;idiff++){
+      for(int idiff=0;idiff<ndiff;idiff++){
 
-      for (int iBin = 1; iBin < fNPoints-1; iBin++) {
-         diffWF[iBin]=(tempWF[iBin+1]-tempWF[iBin-1]);
-         diffNWF[iBin]=(tempNWF[iBin+1]-tempNWF[iBin-1]);
+         for (int iBin = 1; iBin < fNPoints-1; iBin++) {
+            diffWF[iBin]=(tempWF[iBin+1]-tempWF[iBin-1]);
+            diffNWF[iBin]=(tempNWF[iBin+1]-tempNWF[iBin-1]);
+         }
+         diffWF[0]=0;
+         diffWF[fNPoints-1]=0;
+         diffNWF[0]=0;
+         diffNWF[fNPoints-1]=0;
+         memcpy(tempWF,diffWF,fNPoints*sizeof(Double_t) );
+         memcpy(tempNWF,diffNWF,fNPoints*sizeof(Double_t) );
       }
-      diffWF[0]=0;
-      diffWF[fNPoints-1]=0;
-      diffNWF[0]=0;
-      diffNWF[fNPoints-1]=0;
-      memcpy(tempWF,diffWF,fNPoints*sizeof(Double_t) );
-      memcpy(tempNWF,diffNWF,fNPoints*sizeof(Double_t) );
-   }
-   memcpy(fAmplitude,tempWF,fNPoints*sizeof(Double_t) );
-   memcpy(fNoiseAmp,tempNWF,fNPoints*sizeof(Double_t) );
+      memcpy(fAmplitude,tempWF,fNPoints*sizeof(Double_t) );
+      memcpy(fNoiseAmp,tempNWF,fNPoints*sizeof(Double_t) );
    }
 
    // for (int iBin = 0; iBin < fNPoints; iBin++) {
@@ -195,9 +214,9 @@ void Waveform::Differentiate(Int_t ndiff){
    // }
 }
 
-void Waveform::SetNoiseLevel(Double_t noiselevel){
+void Waveform::MakeElectricNoise(){
    for (int ipnt = 0; ipnt < fNPoints; ipnt++) {
-      Double_t noise=gRandom->Gaus(0,noiselevel);
+      Double_t noise=gRandom->Gaus(0,fNoiseLevel);
       // fAmplitude[ipnt]+=noise;
       fNoiseAmp[ipnt]=noise;
    }
@@ -218,7 +237,7 @@ void Waveform::MakeEvent(Int_t npe){ // define npe as initial number of photon
       // Double_t pulsetime=gRandom->Uniform(0,30);
       gFSingle->SetParameter(1, pulsetime);
 
-      for (int iBin = 0; iBin < gNbin; iBin++) {
+      for (int iBin = 0; iBin < Nbins; iBin++) {
          // for (std::vector<Double_t >::iterator it= noiselist.begin();it!=noiselist.end();it++) {
          // Int_t dis=std::distance(noiselist.begin(),it);
          fAmplitude[iBin]+= CT_num*gFSingle->Eval(fTime[iBin]);
@@ -233,12 +252,32 @@ void Waveform::MakeEvent(Int_t npe){ // define npe as initial number of photon
             Double_t decayconst = gFSingle->GetParameter(0);
             Double_t AP_amp = 1.-TMath::Exp(-APtime/decayconst);
             gFSingle->SetParameter(1,pulsetime+APtime);
-            for(int iBin=0; iBin < gNbin; iBin++){
+            for(int iBin=0; iBin < Nbins; iBin++){
                fAmplitude[iBin]+= AP_amp*gFSingle->Eval(fTime[iBin]);
             }
          }
       }
    } // end of loop considering npe
+}
+
+void Waveform::MakeDarkNoise(){
+   //generate Dark Noise from -500 ~ 1500
+
+   Double_t Nexp  = fDNFreq*2000/TMath::Power(10,9);
+   fNDN=gRandom->Poisson(Nexp);
+   // std::cout<<"N DN: "<<fNDN;
+   // Double_t ProbinWindow =
+   for (Int_t iDN = 0; iDN < fNDN; iDN++) {
+      Double_t DNtime=gRandom->Uniform(-500,1500);
+      // std::cout<<" time : "<<DNtime<<std::endl;
+      gFSingle->SetParameter(1,DNtime);
+      for(int iBin=0; iBin < Nbins; iBin++){
+         fAmplitude[iBin]+= gFSingle->Eval(fTime[iBin]);
+      }
+   }
+
+
+
 }
 
 Double_t Waveform::GetCharge(){
